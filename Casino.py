435f22b807444bb4431b8d3a9655470441b2e43a
Jan Serovic <smaxor91@gmail.com>
2015-10-27 17:39:41 +0100

    Casino game

---
+++ Casino.py
@@ -0,0 +1,7 @@
+while True:
+    stevilo = raw_input ("Ugani skrito stevilo...")
+    if stevilo == "7":
+        break
+    print ("Zal ni dobitka, poskusi ponovno!")
+
+print "Cestitamo, zadeli ste dobitek!!"
